e212746ee4324452685c1f92db1843eeed05dba2
Jetmate <antonoutkine@gmail.com>
2018-02-24 22:26:54 -0600

    Implement rope

--- matter.ts
+++ matter.ts
@@ -1,33 +1,24 @@
-import { Engine, World, Bodies, Constraint, Composites, Composite } from 'matter-js'
+import { Engine, World, Bodies, Constraint, Composites, Composite, Body } from 'matter-js'
 import * as SVG from 'svg.js'
+import { KeyEvent } from 'paper';
 
 const ELEM = document.querySelector('#game') as HTMLElement
 const app = SVG(ELEM)
 
 const engine = Engine.create()
 
-// World.add(engine.world, [...Array(5).keys()].map((i) => (
-// 	Bodies.rectangle(i * 10, 0, 10, 10)
-// )))
-
-// const constraint = Constraint.create({
-// 	pointA: { x: 100, y: 30 },
-// 	bodyB: body,
-// 	// pointB: { x: 400, y: 10 },
-// 	// length: 500,
-// 	// stiffness: .1,
-// })
-
 function createRope () {
-	const bodies = []
-	for (let i = 0; i < 5; i++) {
-		const body = Bodies.rectangle(500, 30 + i * 10, 10, 10)
-		app.rect(10, 10).id(body.id.toString())
-		bodies.push(body)
-	}
+	const location = [100, 100]
+	const size = [10, 10]
+	const bodies = [...Array(5).keys()].map(i => (
+		Bodies.rectangle(
+			location[0], location[1] + i * size[1],
+			size[0], size[1],
+		)
+	))
 
-	const composite = Composite.create({ bodies })
-	Composites.chain(composite, 0.5, 0, -0.5, 0, {})
+	const composite = Composite.create({ bodies, label: 'rope' })
+	Composites.chain(composite, -.5, 0, .5, 0, {})
 	Composite.add(composite, Constraint.create({
 		bodyB: composite.bodies[0],
 		// pointB: { x: -20, y: 0 },
@@ -35,17 +26,37 @@ function createRope () {
 		// stiffness: 0.5
 	}))
 	World.add(engine.world, composite)
+	app.path(createPath(bodies)).id(composite.id.toString()).stroke({ width: 3 })
+	// bodies.forEach(body => app.rect(size[0], size[1]).x(body.position.x).y(body.position.y).id(body.id.toString()))
 }
 
 createRope()
 
 Engine.run(engine)
+// Engine.update(engine)
 
 function frame (timestep) {
-	for (const body of engine.world.composites[0].bodies) {
-		app.get(body.id).x(body.position.x).y(body.position.y)
+	for (const composite of engine.world.composites) {
+		if (composite.label.split(' ').includes('rope')) {
+			// composite.bodies.forEach(body => {
+			// 	SVG.get(body.id.toString()).x(body.position.x).y(body.position.y)
+			// })
+			;(SVG.get(composite.id.toString()) as SVG.Path).plot(createPath(composite.bodies))
+		}
 	}
 
 	requestAnimationFrame(frame)
 }
 requestAnimationFrame(frame)
+
+// window.onkeypress = (event: KeyboardEvent) => {
+// 	if (event.key === 'n') {
+// 		Engine.update(engine)
+// 		requestAnimationFrame(frame)
+// 	}
+// }
+
+function createPath (bodies: Body[]) {
+	// if is first body then move the svg otherwise draw a line
+	return bodies.map((body, i) => [!i ? 'M' : 'L', body.position.x, body.position.y])
+}
