9a9978ca854f25899e0179bd31de9ba0ba91cf98
Anton <antonoutkine@gmail.com>
2018-03-19 20:48:25 -0500

    Build

--- src/main2.ts
+++ src/main2.ts
@@ -1,37 +1,4 @@
-import * as SVG from 'svg.js'
-
-const ELEM = document.querySelector('#game')
-
-const SPACE_X = 40
-const SPACE_Y = 40
-
-
-function init (app) {
-	const squares = app.group()
-	for (let x = 0; x <= ELEM.clientWidth / SPACE_X; x++) {
-		for (let y = 0; y <= ELEM.clientWidth / SPACE_Y; y++) {
-			squares
-				.rect(SPACE_X, SPACE_Y)
-				.attr({
-					fill: 'white',
-					stroke: 'rgb(100, 100, 100)',
-					x: SPACE_X * x,
-					y: SPACE_Y * y,
-				})
-		}
-	}
-
-	const colors = ['#DBA159', '#EFD780', '#FCFDAF']
-	squares
-		.skew(10, 3)
-		.scale(1.2)
-		.mousemove(function (this: SVG.G, mouse: any) {
-			this.each(function (this: SVG.Element) {
-				const square = this as SVG.Rect
-				const rect = square.node.getBoundingClientRect()
-				const x = (rect.left + rect.right) / 2
-				const y = (rect.top + rect.bottom) / 2
-				let distance = Math.floor(Math.sqrt(
+import * as SVG from 'svg.jsistance = Math.floor(Math.sqrt(
 					((x - mouse.clientX) / SPACE_X) ** 2 +
 					((y - mouse.clientY) / SPACE_Y) ** 2
 				) - .9)
